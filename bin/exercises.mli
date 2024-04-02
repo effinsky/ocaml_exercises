b070a4ef5f3f8e656e3a63653b0307f97e825d0c
adi forkasiewicz <adi.forkasiewicz@ingrid.com>
2024-04-02 13:16:51 +0200

    added rle modded and cleaned up main file

--- bin/exercises.mli
+++ bin/exercises.mli
@@ -1,10 +1,21 @@
 val is_palindrome : 'a list -> bool
-val encode : 'a list -> (int * 'a) list
-val pack : 'a list -> 'a list list
-val compress : 'a list -> 'a list
 
-type 'a node =
-  | One of 'a
-  | Many of 'a node list
+type 'a node = One of 'a | Many of 'a node list
 
 val flatten : 'a node list -> 'a list
+
+val compress : 'a list -> 'a list
+
+val pack : 'a list -> 'a list list
+
+val encode : 'a list -> (int * 'a) list
+
+val _last_two : 'a list -> ('a * 'a) option
+
+val _nth_element : int -> 'a list -> 'a option
+
+type 'a rle_item = Single of 'a | Plural of int * 'a
+
+val rle_modded : 'a list -> 'a rle_item list
+
+val unknown : 'a list -> int list
--- bin/main.ml
+++ bin/main.ml
@@ -1,35 +1,42 @@
-open Exercises
+let printer (f : 'a -> unit) (lst : 'a list) = List.iter f lst
 
 let () =
+  (* palindrome *)
   let inp = [ 0; 1; 2; 3; 2; 1; 0 ] in
-  is_palindrome inp |> Printf.printf "is the list a palindrome? %b\n";
+  Exercises.is_palindrome inp |> Printf.printf "is the list a palindrome? %b\n";
 
-  let arr = [| 0; 1; 2; 3; 4 |] in
-  Array.set arr 3 666;
-
-  print_endline "printing array";
-  Array.iter (fun it -> print_int it) arr;
-
-  print_endline "";
-
+  (* flatten *)
   let flattened =
-    flatten [ One "a"; Many [ One "b"; Many [ One "c"; One "d" ]; One "e" ] ]
+    Exercises.flatten [ One "a"; Many [ One "b"; Many [ One "c"; One "d" ]; One "e" ] ]
   in
-  List.iter (fun it -> Printf.printf "%s\n" it) flattened;
+  printer (fun it -> Printf.printf "%s\n" it) flattened;
 
+  (* compress *)
   let uncompressed = [ 1; 1; 2; 2; 3; 3; 3; 4; 5; 5 ] in
-  let compressed = compress uncompressed in
-  List.iter (fun it -> Printf.printf "%d\n" it) compressed;
+  let compressed = Exercises.compress uncompressed in
+  printer (fun it -> Printf.printf "%d\n" it) compressed;
 
+  (* pack *)
   let unpacked = [ 1; 1; 2; 2; 3; 4; 4; 4 ] in
-  let packed = pack unpacked in
-  List.iter
+  let packed = Exercises.pack unpacked in
+  printer
     (fun sublist ->
       print_string "new sublist:\n";
       List.iter (fun item -> Printf.printf "%d\n" item) sublist)
     packed;
 
+  (* rlencode *)
   let list_to_encode = [ "a"; "a"; "b"; "b"; "c"; "c"; "c"; "d"; "d" ] in
-  let encoded = encode list_to_encode in
-  List.iter (fun (count, it) -> Printf.printf "%d : %s" count it) encoded
+  let encoded = Exercises.encode list_to_encode in
+  printer (fun (count, it) -> Printf.printf "%d : %s" count it) encoded;
+
+  (* rl_encode modified *)
+  let encoded_modified = Exercises.rle_modded list_to_encode in
+  printer
+    (fun it ->
+      match it with
+      | Exercises.Single x -> Printf.printf "%s" x
+      | Exercises.Plural (count, it) -> Printf.printf "%d : %s" count it)
+    encoded_modified
 ;;
+
